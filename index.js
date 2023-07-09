12afbe2670da18e92d91d914bcbd193f4a8d5c63
Ikhor03 <imamkhoiri725@gmail.com>
2023-07-09 11:02:33 +0700

    connect session sequelize

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import express from "express";
 import cors from "cors";
 import session from "express-session";
 import dotenv from "dotenv";
-// import db from "./config/database.js"
+import sequelizeStore from "connect-session-sequelize";
+import db from "./config/database.js"
 import userRoute from './routes/userRoute.js';
 import productRoute from './routes/productRoute.js';
 import authRoute from "./routes/authRoute.js"
@@ -10,7 +11,10 @@ dotenv.config();
 
 
 const app = express();
+const sessionStore = sequelizeStore(session.Store);
+const store = new sessionStore({db});
 
+//create table from model
 // (async () => {
 //     await db.sync();
 // })()
@@ -25,6 +29,7 @@ app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
+    store : store,
     cookie: {
         secure: 'auto'
     }
@@ -38,6 +43,8 @@ app.get('/', (req, res) => {
         "welcome": "welcome to USER API"
     })
 })
+
+// store.sync(); //create table
 
 app.listen(process.env.APP_PORT, () => {
     console.log('Server up and running.., port ' + process.env.APP_PORT)
