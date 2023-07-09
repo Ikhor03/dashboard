6af959cc07c855375a025356d1828d752fd861bc
Ikhor03 <imamkhoiri725@gmail.com>
2023-07-09 11:42:04 +0700

    add middleware authentication & authorization user

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,13 +6,14 @@ import {
     deleteUser,
     updateUser
 } from "../controllers/user.js";
+import {verifyUser, adminOnly} from "../middleware/authUser.js";
 
 const router = express.Router();
 
-router.get('/user', getUser);
-router.get('/user/:id', getUserById);
-router.post('/user', createUser);
-router.put('/user/:id', updateUser);
-router.delete('/user/:id', deleteUser);
+router.get('/user', verifyUser, adminOnly, getUser);
+router.get('/user/:id', verifyUser, adminOnly, getUserById);
+router.post('/user', verifyUser, adminOnly, createUser);
+router.put('/user/:id', verifyUser, adminOnly, updateUser);
+router.delete('/user/:id', verifyUser, adminOnly, deleteUser);
 
 export default router;
