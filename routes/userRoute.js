747d29f17bc663dacfa4b5d8504c99961d7bc5e4
Ikhor03 <imamkhoiri725@gmail.com>
2023-07-12 07:41:13 +0700

    update feature : register

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -12,7 +12,7 @@ const router = express.Router();
 
 router.get('/user', verifyUser, adminOnly, getUser);
 router.get('/user/:id', verifyUser, adminOnly, getUserById);
-router.post('/user', verifyUser, adminOnly, createUser);
+router.post('/user', createUser);
 router.put('/user/:id', verifyUser, adminOnly, updateUser);
 router.delete('/user/:id', verifyUser, adminOnly, deleteUser);
 
